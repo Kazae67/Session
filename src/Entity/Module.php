36f48a8f8810b4ae82481a334eb0e5730eb3c7c7
Kazae67 <Kazae.zaeka@gmail.com>
2023-08-24 09:50:42 +0200

    ajout d'unicité pour éviter les doublons dans module

--- src/Entity/Module.php
+++ src/Entity/Module.php
@@ -6,8 +6,10 @@ use App\Repository\ModuleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: ModuleRepository::class)]
+#[UniqueEntity("labelModule")]
 class Module
 {
     #[ORM\Id]
