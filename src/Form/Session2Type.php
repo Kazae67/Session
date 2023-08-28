23fcefc01f61ae71cb47fea415bdedd2a49ab7a0
Kazae67 <Kazae.zaeka@gmail.com>
2023-08-28 02:34:41 +0200

    modification des dates

--- src/Form/Session2Type.php
+++ src/Form/Session2Type.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Session;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +14,16 @@ class Session2Type extends AbstractType
     {
         $builder
             ->add('label')
-            ->add('dateStart')
-            ->add('dateEnd')
+            ->add('dateStart', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
+            ->add('dateEnd', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
             ->add('nbLimit')
             ->add('stagiaires')
-
         ;
     }
 
